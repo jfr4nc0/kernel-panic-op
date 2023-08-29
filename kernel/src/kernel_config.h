73f4c7646e7fb2d2ecc6d71e9b1a1ea389911240
jfr4nc0 <jfcpersonal99@gmail.com>
2023-08-29 21:01:33 +0000

    modularizando loggings y configs

---
+++ kernel/src/kernel_config.h
@@ -0,0 +1,12 @@
+#ifndef KERNEL_CONFIG_H_
+#define KERNEL_CONFIG_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <commons/collections/dictionary.h>
+
+
+
+
+#endif
---
+++ utils/src/logs/logs.h
@@ -0,0 +1,6 @@
+#ifndef LOGS_H_
+#define LOGS_H_
+
+
+
+#endif
